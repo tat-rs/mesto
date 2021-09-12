ab94e2fc37dad0e97e6deaf2d39ae54a2df45498
Tatiana Roslyakova <tat-rs@yandex.ru>
2021-09-13 02:44:10 +0300

    отредактирована кнопка в блоке profile и сделано сохрание данных в форме

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,12 @@
-const popup = document.querySelector('.popup');
-const popupOpenBtn = document.querySelector('.profile__link');
-const popupCloseBtn = popup.querySelector('.popup__close');
-let infoContainer = document.querySelector('.form__item')
+let popup = document.querySelector('.popup');
+let popupOpenBtn = document.querySelector('.profile__link');
+let popupCloseBtn = popup.querySelector('.popup__close');
+let pfofileContainer = document.querySelector('.profile__info');
+let profileName = pfofileContainer.querySelector('.profile__title');
+let profileDesc = pfofileContainer.querySelector('.profile__text');
+let formInfo = popup.querySelector('.form');
+let formInfoName = formInfo.querySelector('.form__item_type_name');
+let formInfoDesc = formInfo.querySelector('.form__item_type_desc');
 
 function popupToggle() {
   popup.classList.toggle('popup__opened')
@@ -18,4 +23,12 @@ popup.addEventListener ('click', clickOverlay);
 popupOpenBtn.addEventListener('click', popupToggle);
 popupCloseBtn.addEventListener('click', popupToggle);
 
+formInfoName.value = profileName.textContent;
+formInfoDesc.value = profileDesc.textContent;
 
+function addInfo(evt) {
+  evt.preventDefault()
+  profileName.innerHTML = `<h1 class="profile__title">${formInfoName.value}</h1>`;
+  profileDesc.innerHTML = `<p class="profile__text">${formInfoDesc.value}</p>`;
+}
+formInfo.addEventListener('submit', addInfo);
