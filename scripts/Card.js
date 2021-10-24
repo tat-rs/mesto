dcd5033e54a791a38b9bdaaca769d9778a0d4066
Tatiana Roslyakova <tat-rs@yandex.ru>
2021-10-24 20:39:56 +0300

    реализован класс Card

---
+++ scripts/Card.js
@@ -0,0 +1,59 @@
+import { openPopup } from "./index.js";
+
+export class Card {
+  constructor(data, selector) {
+    this._name = data.name;
+    this._link = data.link;
+    this._selector = selector;
+  }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._selector)
+      .content
+      .querySelector('.cards__item')
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  createCard() {
+    this._element = this._getTemplate();
+
+    this._cardImage = this._element.querySelector('.cards__image');
+    this._cardSubtitle = this._element.querySelector('.cards__subtitle');
+
+    this._setEventListeners();
+
+    this._element.querySelector('.cards__subtitle').textContent = this._name;
+    //присваиваем значение ссылки на изображение новой карточки равной ключу link массива
+    this._cardImage.src = this._link; // добавили адрес изображение из массива карточек
+    this._cardImage.alt = this._name; // добавили описание в атрибут alt, равное названию карточки
+    return this._element;
+  }
+
+  _toggleLike() {
+    this._element.querySelector('.cards__button').classList.toggle('cards__button_active'); //добавляем или убираем класс cards__button_active у элемента
+  }
+
+  _deleteCard() {
+    this._element.closest('.cards__item').remove(); //удаляем блок карточки
+  }
+
+  _showImage() {
+    const popupOpenImage = document.querySelector('.popup_type_image'); //попап просмотра изображения
+    const popupImageName = popupOpenImage.querySelector('.popup__subtitle'); //поле ввода описания картинки в попапе просмотра изображения
+    const popupImageLink = popupOpenImage.querySelector('.popup__image'); //поле ввода ссылки на картинку в попапе просмотра изображения
+    openPopup(popupOpenImage)
+      popupImageName.textContent = this._name; //приравниваем текстовые содержания
+      popupImageLink.src = this._link; //приравниваем ссылки на изображения
+      popupImageLink.alt = this._name; //заполняем альт названием карточки
+  }
+
+  _setEventListeners() {
+    this._element.querySelector('.cards__button').addEventListener('click', () => this._toggleLike());
+    this._element.querySelector('.cards__delete').addEventListener('click', () => this._deleteCard());//добавили слушатель для удаления карточки
+    this._cardImage.addEventListener('click', () => this._showImage());
+  }
+
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-import { initialCards } from "./initial-cards.js"; // импорт массива с первоначальными карточками страницы
+import { Card } from './Card.js';
+import { initialCards } from './initial-cards.js'
 
 const popupEditProfile = document.querySelector('.popup_type_edit'); //переменная попап с формой редактированя профиля
 const popupOpenBtn = document.querySelector('.profile__edit'); //переменная кнопки редактирования профиля
@@ -9,7 +10,6 @@ const formInfo = popupEditProfile.querySelector('.form'); //переменная
 const formInfoName = formInfo.querySelector('.form__item_type_name'); //переменная значения имени профиля
 const formInfoDesc = formInfo.querySelector('.form__item_type_desc'); //переменная значения описания профиля
 
-const cardTemplateElement = document.querySelector('.cards-template');//контейнер template карточек
 const cardListElement = document.querySelector('.cards__list'); //контейнер карточек в разметке
 const popupCreateCard = document.querySelector('.popup_type_new-card'); //попап с добавлением новой картчоки
 const popupCardOpenBtn = document.querySelector('.profile__button'); //кнопка открытия попапа добавления карточки
@@ -19,8 +19,7 @@ const formCardSubtitle = formCard.querySelector('.form__item_type_image-subtitle
 const formImageLink = formCard.querySelector('.form__item_type_image-link'); //поле ввода ссылки на изображение карточки
 const popupOpenImage = document.querySelector('.popup_type_image'); //попап просмотра изображения
 const popupOpenImageCloseBtn = popupOpenImage.querySelector('.popup__close'); //кнопка закрытия попапа просмотра изображения
-const popupImageName = popupOpenImage.querySelector('.popup__subtitle'); //поле ввода описания картинки в попапе просмотра изображения
-const popupImageLink = popupOpenImage.querySelector('.popup__image'); //поле ввода ссылки на картинку в попапе просмотра изображения
+
 
 //функция закрытия попапа по кнопке esc
 function closePopupByEsc (evt) {
@@ -39,7 +38,7 @@ function closePopupByOverlayClick(evt) {
   }
 }
 
-//скрыть текст ошибки валидации
+/* //скрыть текст ошибки валидации
 const hideError = (modal) => {
   const errorTextElements = Array.from(modal.querySelectorAll('.form__error')); //сохраняем массив из элементов ошибки
   //перебираем элементы массива и обнуляем текстовое содержание
@@ -55,10 +54,10 @@ const hideErrorStyle = (modal) => {
   inputElements.forEach((inputElement) => {
     inputElement.classList.remove('form__item_state_invalid');
   })
-}
+} */
 
 //объявление функции открытия попапа
-function openPopup(modal) {
+export function openPopup(modal) {
   modal.classList.add('popup_opened'); //присваиваем класс модификатора popup_opened
   window.addEventListener('keydown', closePopupByEsc); //присваиваем обработчик закрытия
   modal.addEventListener('mousedown', closePopupByOverlayClick); //присваиваем обработчик закрытия попапа по оверлею
@@ -74,8 +73,8 @@ function closePopup(modal) {
 //объявление функции открытия попапа редактирования профиля
 function openEditProfilePopup() {
   openPopup(popupEditProfile);
-  hideError(popupEditProfile);//удалить текст ошибки
-  hideErrorStyle(popupEditProfile); //удалить стили ошибки
+  /* hideError(popupEditProfile);//удалить текст ошибки
+  hideErrorStyle(popupEditProfile); //удалить стили ошибки */
   formInfoName.value = profileName.textContent; //в поле ввода текст со страницы
   formInfoDesc.value = profileDesc.textContent;
 }
@@ -83,8 +82,8 @@ function openEditProfilePopup() {
 //функция открытия попапа добавления новой карточки
 function openCreateCardPopup() {
   openPopup(popupCreateCard);
-  hideError(popupCreateCard); //удалить текст ошибки
-  hideErrorStyle(popupCreateCard); //удалить стили ошибки
+  /* hideError(popupCreateCard); //удалить текст ошибки
+  hideErrorStyle(popupCreateCard); //удалить стили ошибки */
   formCardSubtitle.value = ''; //обнуляем текст
   formImageLink.value = ''; //обнуляем ссылку
 }
@@ -95,49 +94,6 @@ function submitEditProfileForm(evt) {
   profileName.textContent = formInfoName.value;
   profileDesc.textContent = formInfoDesc.value;
   closePopup(popupEditProfile);
-}
-
-//изменяем цвет лайка при нажатии
-function toggleLike(event) {
-  event.target.classList.toggle('cards__button_active'); //добавляем или убираем класс cards__button_active у элемента
-}
-
-//удаление карточки
-function deleteCard(event) {
-  const cardDeleted = event.currentTarget.closest('.cards__item'); //возвращаем ближайший родительский элемент - template
-  cardDeleted.remove(); //удаляем блок карточки
-}
-
-//открытие попапа просмотра изображения
-function showImage(event) {
-  openPopup(popupOpenImage)
-  const mainElement = event.currentTarget.closest('.cards__item'); //возвращаем ближайщий родительский элемент
-  const namePicture = mainElement.querySelector('.cards__subtitle'); // переменная с названием карточки
-  popupImageName.textContent = namePicture.textContent; //приравниваем текстовые содержания
-  popupImageLink.src = mainElement.querySelector('.cards__image').src; //приравниваем ссылки на изображения
-  popupImageLink.alt = namePicture.textContent; //заполняем альт названием карточки
-}
-
-//функция создания новой карточки
-function createCard(element) {
-  const newCardElement = cardTemplateElement.content.cloneNode(true);//клонируем массив
-  //присваиваем значение описания новой карточки равной ключу name массива
-  const newCardImage = newCardElement.querySelector('.cards__image');// переменная с элементом изображение
-
-  newCardElement.querySelector('.cards__subtitle').textContent = element.name;
-  //присваиваем значение ссылки на изображение новой карточки равной ключу link массива
-  newCardImage.src = element.link; // добавили адрес изображение из массива карточек
-  newCardImage.alt = element.name; // добавили описание в атрибут alt, равное названию карточки
-  newCardElement.querySelector('.cards__button').addEventListener('click', toggleLike);//добавили слушатель на кнопку лайк
-  newCardElement.querySelector('.cards__delete').addEventListener('click', deleteCard);//добавили слушатель для удаления карточки
-  newCardImage.addEventListener('click', showImage);//добавили обработчик на картинку, чтобы открывался попап с данным изображением
-  return newCardElement
-}
-
-//функция добавления карточек
-function renderCard(element) {
-  const newElement = createCard(element)
-  cardListElement.prepend(newElement);
 }
 
 //функция дезактивации кнопки сабмита
@@ -152,12 +108,14 @@ function createNewCard(event) {
   event.preventDefault();
 
 //объявляем объект с ключами равными значениям в полях ввода
-  const newCards = {
+  const cardsElement = {
     name: formCardSubtitle.value,
     link: formImageLink.value
   };
 
-  renderCard(newCards); //отображение новой карточки
+  const newElement = renderCard(cardsElement); //отображение новой карточки
+
+  cardListElement.prepend(newElement)
 
   closePopup(popupCreateCard);
 
@@ -166,7 +124,17 @@ function createNewCard(event) {
   event.currentTarget.reset(); //обнуление значение полей ввода
 }
 
-initialCards.map(renderCard) //отображаем новый массив карточек на странице
+//функция добавления карточек
+function renderCard(item) {
+  const card = new Card(item, '.cards-template');
+  const newCard = card.createCard()
+  return newCard
+}
+
+initialCards.forEach((item) => {
+  const element = renderCard(item);
+  cardListElement.prepend(element);
+});
 
 popupOpenBtn.addEventListener('click', openEditProfilePopup);//открытие попапа по клику на кнопку редактирования профиля
 popupCloseBtn.addEventListener('click', () => closePopup(popupEditProfile));//закрытие попапа по клику на кнопку редактирования профиля
