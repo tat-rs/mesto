cf2967edd337d792cb9f00e93202a0a0e0611ad8
Tatiana Roslyakova <tat-rs@yandex.ru>
2021-11-12 21:38:34 +0300

    добавлены комментарии в классе Попап

--- components/Popup.js
+++ components/Popup.js
@@ -9,13 +9,13 @@ export default class Popup {
   //метод открытия попапа
   open() {
     this._popup.classList.add('popup_opened');
-    window.addEventListener('keydown', this._listenerBind)
+    window.addEventListener('keydown', this._listenerBind); //добавили слушатель закрытия по кнопке esc
   }
 
   //метод закрытия попапа
   close() {
     this._popup.classList.remove('popup_opened');
-    window.removeEventListener('keydown', this._listenerBind);
+    window.removeEventListener('keydown', this._listenerBind); //удалили слушатель закрытия по кнопке esc
   }
 
   //логика закрытия попапа по кнопке esc
@@ -28,8 +28,10 @@ export default class Popup {
   //слушатель клика иконки закрытия попапа и по области вокруг
   setEventListeners() {
     this._popup.addEventListener('click', (evt) => {
+      //закрытие по оверлею
       if(evt.target.classList.contains('popup_opened')) {
         this.close();
+      //закрытие по крестику
       } if(evt.target.classList.contains('popup__close')) {
         this.close();
       }
