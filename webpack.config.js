53463e7ee066652bbd506470a779af752e844f06
Tatiana Roslyakova <tat-rs@yandex.ru>
2021-11-21 10:20:55 +0300

    в папке pages файл стилей и js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'development',
-  entry: './src/scripts/index.js',
+  entry: './src/pages/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
